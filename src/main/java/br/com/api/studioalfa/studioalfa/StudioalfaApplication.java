794390f4213dff468bbb575cba1ecdc116620680
fakynno <fakynno@gmail.com>
2023-09-03 22:45:26 -0300

    Importe inicial do projeto

---
+++ src/main/java/br/com/api/studioalfa/studioalfa/StudioalfaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.studioalfa.studioalfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioalfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioalfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/studioalfa/studioalfa/StudioalfaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.studioalfa.studioalfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioalfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
